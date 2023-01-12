4469a4cbf5368efa5fc434323790b042e90ca1db
hyKim <hayou94@gmail.com>
2023-01-12 16:07:43 +0900

    init repo

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+fn main() {
+    // Login with a bot token from the environment
+    let mut client = Client::new(&std::env::var("DISCORD_TOKEN").expect("token"), Handler).expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("Client error: {:?}", why);
+    }
+}
